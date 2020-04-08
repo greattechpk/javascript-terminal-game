7b8e919efd384792b6a48e12a63df9b1fe369813
greattechpk <great.techpk@gmail.com>
2020-04-08 18:44:42 -0400

    made adding folders pass current keys working on creatin function to loop through and make an array of keys

--- js/app.js
+++ js/app.js
@@ -22,16 +22,24 @@ let objFolderTree = {
     etc:{}
 }
 
-const source = document.querySelector("#source")
+const sourceHTML = document.querySelector("#source")
+const destinationHTML = document.querySelector("#destination")
 
+let sourceKeys = []
+let destinationKeys = []
 console.log(source)
 
 
 
 const folderTreeKeyArr = []
 
-function buildTreeKeyArr(obj){
-    const objFolderArr = Object.keys(obj)
+
+//add folders and change keys
+
+function renderItems(view,current,obj){
+    const objFolderArr = Object.keys(obj)//needs to change to current directory
+    //resets folders before adding new
+    view.innerHTML = ''
     console.log(objFolderArr)
     objFolderArr.forEach(item => {
         //creating folder with attributes
@@ -39,14 +47,32 @@ function buildTreeKeyArr(obj){
         folderViz.classList.add('folder')
         //creating adding Icon
         let folderIcon = document.createElement("img")
-        folderIcon.src = ""
-
-        
+        folderIcon.src = "/img/folder_BW.png"
+        folderIcon.height = "96"
         folderViz.appendChild(folderIcon)
-        console.log(folderViz)  
-        
+        //adding text
+        let text = document.createTextNode(item)
+        folderViz.appendChild(text)
+        view.appendChild(folderViz)
     })
-
+    
+    current = obj
+    if (view === sourceHTML){
+        sourceKeys = Object.keys(current)
+    }
+    if (view === destinationHTML){
+        destinationKeys = Object.keys(current)
+    }
 }
 
-buildTreeKeyArr(objFolderTree)
+function ls(){
+    
+}
+
+renderItems(sourceHTML,sourceKeys,objFolderTree)
+
+renderItems(destinationHTML,destinationKeys,objFolderTree)
+
+
+console.log(sourceKeys)
+console.log(destinationKeys)
